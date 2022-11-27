256c126f69d8b069d8c19775ee6018c0714fbb7f
Serg <serg21322@yandex.ru>
2022-11-27 23:49:50 +0300

    Recomplete OOP_dunder_method_task7 of getitem_setitem

--- OOP_Dunder_methods/getitem_setitem_delitem/OOP_dunder_method_task7.py
+++ OOP_Dunder_methods/getitem_setitem_delitem/OOP_dunder_method_task7.py
@@ -1,4 +1,6 @@
 class Cell:
+    """A class that describes a square of the playing field in tic-tac-toe"""
+
     def __init__(self) -> None:
         self.is_free = True
         self.value = 0
@@ -8,34 +10,53 @@ class Cell:
 
 
 class TicTacToe:
+    """Game pole class"""
+
     def __init__(self) -> None:
+        # number of cells
         self._n = 3
-        self.pole = list(list(Cell() for _ in range(self._n)) for _ in range(self._n))
+
+        # playing field represented as a two-dimensional tuple
+        self.pole = tuple(tuple(Cell() for col in range(self._n)) for row in range(self._n))
 
     def clear(self) -> None:
-        for row in self.pole:
-            for cell in row:
-                cell.is_free = True
-                cell.value = 0
+        """Clears the playing field (removes all tic-tac-toe, closes cells)"""
 
-    def check_index(self, item) -> None:
+        self.__init__()
+
+    def __check_index(self, item: tuple) -> None:
+        """Checks that the coordinate does not go beyond the size of the field"""
+
         if type(item) != tuple or len(item) != 2:
             raise IndexError("неверный индекс клетки")
         if any(not (0 <= x < self._n) for x in item if type(x) != slice):
             raise IndexError("неверный индекс клетки")
 
-    def __getitem__(self, item):
-        self.check_index(item)
-        row, col = item
-        if type(row) == slice:
-            return tuple(self.pole[x][col] for x in range(self._n))
-        if type(col) == slice:
-            return tuple(self.pole[row][x] for x in range(self._n))
+    def __getitem__(self, item: tuple):
+        """Getting value by x, y coordinates (one of the coordinates can be
+        represented by a slice. In this case, all values located at the coordinate
+        represented by an integer are selected). If both coordinates
+        are integers - just return the element of the field located on
+        corresponding coordinate"""
 
+        self.__check_index(item)
+        row, col = item
+
+        if type(row) == slice:
+            # All cells under the row column are returned
+            return tuple(self.pole[x][col].value for x in range(self._n))
+
+        if type(col) == slice:
+            # All cells under the col column are returned
+            return tuple(self.pole[row][x].value for x in range(self._n))
+
+        # Returning a specific cell
         return self.pole[row][col].value
 
-    def __setitem__(self, key, value: int):
-        self.check_index(key)
+    def __setitem__(self, key, value):
+        """Sets the value of a specific cell of the playing field"""
+
+        self.__check_index(key)
         row, col = key
         if self.pole[row][col]:
             self.pole[row][col].value = value
@@ -49,6 +70,7 @@ def main() -> None:
     game.clear()
     game[0, 0] = 1
     game[1, 0] = 2
+    game[2, 0] = 3
     # формируется поле:
     # 1 0 0
     # 2 0 0
@@ -56,8 +78,10 @@ def main() -> None:
     # game[3, 2] = 2  # генерируется исключение IndexError
     if game[0, 0] == 0:
         game[0, 0] = 2
-    v1 = game[0, :]  # 1, 0, 0
+    v1 = game[:, 0]  # 1, 0, 0
     v2 = game[:, 0]  # 1, 2, 0
+    for i in v1:
+        print(i.value)
 
 
 if __name__ == '__main__':
