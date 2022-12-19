ec602a26eac4494eacf0ecccdd4d78857f30d751
Serg <serg21322@yandex.ru>
2022-12-20 02:50:10 +0300

    recomplete Multiple_Inheritance_task3.py

--- OOP_inheritance_polymorphism/Multiple_Inheritance/Multiple_Inheritance_task3.py
+++ OOP_inheritance_polymorphism/Multiple_Inheritance/Multiple_Inheritance_task3.py
@@ -2,27 +2,28 @@ from typing import Dict
 
 
 class RetriveMixin:
-    def get(self, request):
+    def get(self, request: dict) -> str:
         return "GET: " + request.get('url')
 
 
 class CreateMixin:
-    def post(self, request):
+    def post(self, request: dict) -> str:
         return "POST: " + request.get('url')
 
 
 class UpdateMixin:
-    def put(self, request):
+    def put(self, request: dict) -> str:
         return "PUT: " + request.get('url')
 
 
 class GeneralView:
     allowed_methods = ('GET', 'POST', 'PUT', 'DELETE')
 
-    def render_request(self, request: Dict[str, str]):
+    def render_request(self, request: Dict[str, str]) -> str:
         if request.get('method') not in self.allowed_methods:
             raise TypeError(f"Метод {request.get('method')} не разрешен.")
         else:
+            """Getting a reference to a method by name and returning a request """
             method_request = request.get('method').lower()
             return getattr(self, method_request)(request)
 
